5012b4ba0f02ae85b00d0ab20966f0c56de87fa5
rloutier <roloutier-ext@airfrance.fr>
2024-04-04 15:09:21 +0200

    Switch to @WebServlet for URL mappings
    Much more concise and readable, servlet 3.0 needed in maven conf file.
    web.xml is now useless, but kept as I might need it later.

--- src/main/java/servlets/FirstServlet.java
+++ src/main/java/servlets/FirstServlet.java
@@ -3,12 +3,14 @@ package servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import beans.Author;
 
+@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String customMessage = "Yet another message here!";
--- src/main/java/servlets/WelcomeServlet.java
+++ src/main/java/servlets/WelcomeServlet.java
@@ -3,10 +3,12 @@ package servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/")
 public class WelcomeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher("/WEB-INF/welcome.jsp").forward(request, response);
